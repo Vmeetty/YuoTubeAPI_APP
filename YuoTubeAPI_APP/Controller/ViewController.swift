c94db610b8ada042cdfa5814f91db547e8b7171b
Vlad Ch <vividimagelogin@gmail.com>
2022-06-17 14:01:19 +0300

    pageControl and gallery slider

--- YuoTubeAPI_APP/Controller/ViewController.swift
+++ YuoTubeAPI_APP/Controller/ViewController.swift
@@ -10,38 +10,71 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var galleryCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
-    private let galleryCollectionView = GalleryCollectionView()
     private let playlistCollectionView = PlaylistCollectionView()
+    
+    var galleryCells = VideoModel.fetchVideo()
+    
+    var timer: Timer?
+    var currentIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
-        view.addSubview(galleryCollectionView)
+        galleryCollectionView.delegate = self
+        galleryCollectionView.dataSource = self
+        pageControl.numberOfPages = galleryCells.count
         view.addSubview(playlistCollectionView)
-        
-        //MARK: - GalleryCollectionView constraints
-        galleryCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        galleryCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        galleryCollectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
-        galleryCollectionView.heightAnchor.constraint(equalToConstant: 250).isActive = true
-        
-        galleryCollectionView.set(cells: VideoModel.fetchVideo())
-        
+       
         //MARK: - PlaylistCollectionView constraints
         playlistCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         playlistCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        playlistCollectionView.topAnchor.constraint(equalTo: galleryCollectionView.bottomAnchor, constant: 100).isActive = true
+        playlistCollectionView.topAnchor.constraint(equalTo: pageControl.bottomAnchor, constant: 40).isActive = true
         playlistCollectionView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
         playlistCollectionView.set(cells: VideoModel.fetchVideo())
-        
-        galleryCollectionView.startTimer()
+        startTimer()
     }
 
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(moveToTheNextIndex), userInfo: nil, repeats: true)
+    }
     
-    
+    @objc func moveToTheNextIndex() {
+        if currentIndex < galleryCells.count - 1 {
+            currentIndex += 1
+        } else {
+            currentIndex = 0
+        }
+        galleryCollectionView.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .right, animated: true)
+        pageControl.currentPage = currentIndex
+    }
 
 }
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return galleryCells.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = galleryCollectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCollectionViewCell", for: indexPath) as! GalleryCollectionViewCell
+        cell.mainImageView.image = galleryCells[indexPath.row].image
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: galleryCollectionView.frame.width, height: galleryCollectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
 
